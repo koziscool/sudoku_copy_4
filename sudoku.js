73956d53fc0d89e0ebcdfd806a197327690f7aac
koziscool <kozjob@yahoo.com>
2016-07-04 12:17:55 -0500

    stuff

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,28 @@
 
 
-var innerBlock = function(){
+var test_grid2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
+
+var gridIndex = function( outerRow, outerCol, innerRow, innerCol ) {
+  var row = 3*outerRow + innerRow;
+  var col = 3*outerCol + innerCol;
+  return 3*row + col;
+};
+
+var innerBlock = function( outerRow, outerCol ){
   var $table = $('<table>').addClass('inner-block');
   for (var i = 0; i < 3 ; i++ ) {
     var $row = $('<tr>').addClass('inner-block-row');
     for (var j = 0; j < 3 ; j++ ) {
-     var $col = $('<td>').addClass('inner-block-data');
+      var $col = $('<td>').addClass('inner-block-data');
      // $col.append( innerBlock(i, j) ); 
-     var cellData = "."
+      var index = gridIndex( outerRow, outerCol, i, j );
+      var cellData = test_grid2[index];
+      if(cellData === ".")
+        cellData = "";
 
-     $col.append('<div>.</div>')
+      var inputString = '<input type="text" maxlength="1" size="1" class="cell-input" value="' + cellData + '">';
+      var $input = $(inputString);
+     $col.append($input);
      $row.append($col)  
     }
   $table.append($row);
@@ -19,7 +32,6 @@ var innerBlock = function(){
 
 
 var outerBlock = function(){
-  console.log("here");
   var $table = $('<table>').addClass('outer-block');
   for (var i = 0; i < 3 ; i++ ) {
     var $row = $('<tr>').addClass('outer-block-row');
@@ -36,9 +48,7 @@ var outerBlock = function(){
 
 
 var displayGrid = function() {
-
   $grid = $('#puzzle-grid');
-  // $grid.append('<div>koz is cool</div>')
   $grid.append( outerBlock() );
 };
 
